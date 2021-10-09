1a4c331831b2ac7980057dc08c1afd0938a81558
Your Name <you@example.com>
2021-10-09 14:44:10 -0700

    commit

--- data.cs
+++ data.cs
@@ -1,39 +1,20 @@
 using System;
 using System.IO;
-using System.Collections.Generic; 
 
 namespace ConsoleApp5
 {
-    public partial class Program
+    class Program
     {
-        public class StoreData
+        static void Main(string[] args)
         {
-            public void ReadData()
-            {
-                int storeID
-                int chosenDate
-                double weeklySales
-                int isHoliday
-
-                public StoreData()
-                {
-                    storeID = lineContent[0]
-                    chosenDate = lineContent[1]
-                    weeklySales = lineContent[2]
-                    isHoliday = lineContent[3]
-                }
-
-                const string FILE = @"C:\Users\immer\Desktop\gitAssignment1\C_Sharp_A1\Walmart_Store_Data.csv";
-                StreamReader sr = new StreamReader(FILE);
-                while (!sr.EndOfStream) {
+            const string FILE = @"C:\Users\immer\Desktop\gitAssignment1\C_Sharp_A1\Walmart_Store_Data.csv";
+            StreamReader sr = new StreamReader(FILE);
+            while (!sr.EndOfStream) {
                 string lineContent = sr.ReadLine();
                 Console.WriteLine(lineContent);
-                }
-                sr.Close(); // It is better to close the reader when finished.
-                Console.ReadLine();
-                
             }
-            
+            sr.Close(); // It is better to close the reader when finished.
+            Console.ReadLine();
         }
     }
 }
