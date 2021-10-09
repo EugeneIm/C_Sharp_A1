0458498b2f07f8c8ce02a07c074babc3f304b364
EugeneIm <immer277@gmail.com>
2021-10-09 13:36:21 -0700

    commit

--- data.cs
+++ data.cs
@@ -12,9 +12,11 @@ namespace ConsoleApp5
             {
 
                 string rowCSV = File.ReadAllText(@"/Users/eugene/Desktop/CA1/Walmart_Store_Data.csv");
-                Console.WriteLine(rowCSV);
+                Console.WriteLine(rowCSV[0]);
                 Console.ReadLine();
-
+                Console.WriteLine("Here is some dummy text to replace the data menu");
+                Console.WriteLine($"If the entered ID matches the ID take all the values and sum them all up");
+                
             }
             
         }
